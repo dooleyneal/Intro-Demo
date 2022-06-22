129987331d54106725940975b2db8955bc393c74
Neal Dooley <nealdooley19@gmail.com>
2022-06-22 12:35:02 -0500

    Initial commit

---
+++ Intro-Demo.js
@@ -0,0 +1,78 @@
+let harryAttack = 25;
+let lordVAttack = 35;
+
+if(harryAttack > lordVAttack){
+    console.log ("Harry has a better attack than Lord V");
+}
+else if(harryAttack <lordVAttack) {
+    console.log("Lord V has a better attack than Harry");
+}
+else{
+    console.log("Harry and Lord V have the same attack.");
+}
+
+let harryHealth = 100;
+let harryDefense = 0;
+
+if (harryHealth <= lordVAttack){
+    console.log("Harry has been Slain.")
+}
+else {
+    harryHealth -= lordVAttack
+    console.log(`Harry's health is down ${harryHealth}`)
+}
+
+//Harry picks up a shield
+harryDefense += 25;
+
+if(harryHealth <= lordVAttack-harryDefense){
+    console.log("Harry has been slain!")
+}
+else {
+    harryHealth -= (lordVAttack - harryDefense)
+    console.log(`Harry's health is down ${harryHealth}`)
+}
+
+let healthKit = 50;
+
+if((harryHealth + healthKit) >= 100){
+    harryHealth = 100;
+}
+else {
+    harryHealth += healthKit;
+}
+console.log(`Harry got some help. Health is now ${harryHealth}`)
+
+
+let coinTossHeads = false;
+
+if(coinTossHeads === true) {
+    console.log ("The fight continues")
+}
+else {
+    console.log("Harry is allowed to run away")
+}
+
+// for (let i = 0; i <= 10; i++){
+//     if (harryHealth <=0){
+//         console.log("Harry has been slain")
+//     }
+//     else {
+//         harryHealth -= (lordVAttack -harryDefense)
+//         console.log(`Harry's health is ${harryHealth}`)
+
+//     }
+// }
+
+while (harryHealth >0) {
+    harryHealth -= (lordVAttack -harryDefense)
+    console.log(`Harry's health is ${harryHealth}`)
+
+    if (harryHealth <=0) {
+        console.log("Harry has been slain")
+    }
+}
+
+
+
+
